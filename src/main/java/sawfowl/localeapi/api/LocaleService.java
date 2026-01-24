f1f3ddd34e3c604652efd2a00f3f347fb183feba
Иван <StalkerIv@yandex.ru>
2026-01-24 23:59:35 +0300

    Update LocaleService.java
    Add comments

--- src/main/java/sawfowl/localeapi/api/LocaleService.java
+++ src/main/java/sawfowl/localeapi/api/LocaleService.java
@@ -86,16 +86,45 @@ public abstract class LocaleService {
 	 */
 	public abstract <T extends Translation> Class<T> getDefaultReference(String pluginID);
 
+	/**
+	 * Create and register localizations of your plugin.<br>
+	 * Previously saved localizations will be uploaded automatically.<br>
+	 * Localization files from your plugin's jar file will also be automatically saved and loaded.
+	 * 
+	 * @param container - Your plugin container.
+	 */
 	public abstract <T extends Translation> LocalesList<T> createLocales(PluginContainer container);
 
+	/**
+	 * 
+	 * Create and register localizations of your plugin.<br>
+	 * Previously saved localizations will be uploaded automatically.<br>
+	 * Localization files from your plugin's jar file will also be automatically saved and loaded.
+	 * 
+	 * @param container - Your plugin container.
+	 * @param translationReference  - Your serializable class for localizations. Do not specify interfaces and abstract classes here. Your class must be serializable.
+	 * @return A collection of localizations of your plugin with the type you need. The type can be an interface or an abstract class.
+	 */
 	public abstract <T extends Translation> LocalesList<T> createLocales(PluginContainer container, Class<? extends T> translationReference);
 
+	/**
+	 * @return A collection of localizations of your plugin with the type you need. The type can be an interface or an abstract class.
+	 */
 	public abstract <T extends Translation> LocalesList<T> getLocales(PluginContainer container);
 
+	/**
+	 * @return A collection of localizations of your plugin with the type you need. The type can be an interface or an abstract class.
+	 */
 	public abstract <T extends Translation> LocalesList<T> getLocales(String plugin);
 
+	/**
+	 * Checking whether the collection of localizations for the specified plugin is registered.
+	 */
 	public abstract boolean localesExist(PluginContainer container);
 
+	/**
+	 * Checking whether the collection of localizations for the specified plugin is registered.
+	 */
 	public abstract boolean localesExist(String plugin);
 
 }

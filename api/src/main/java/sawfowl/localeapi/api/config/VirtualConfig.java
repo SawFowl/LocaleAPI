516a02c61d56826869c27a501692d45bc5eefadf
Иван <StalkerIv@yandex.ru>
2026-03-30 22:04:46 +0300

    Update VirtualConfig.java

--- api/src/main/java/sawfowl/localeapi/api/config/VirtualConfig.java
+++ api/src/main/java/sawfowl/localeapi/api/config/VirtualConfig.java
@@ -17,7 +17,7 @@ import sawfowl.localeapi.api.ConfigTypes;
 public interface VirtualConfig {
 
 	/**
-	 * @return The path to the configuration file.
+	 * @return The method returns raw data, either the original data entered when the configuration was created, or new data after the configuration was modified and saved.
 	 */
 	String getRawData();
 

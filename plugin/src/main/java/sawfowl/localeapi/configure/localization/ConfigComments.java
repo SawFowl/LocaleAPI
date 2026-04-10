f994cf4da81688d08287783f303afcdda71605e6
Иван <StalkerIv@yandex.ru>
2026-04-10 15:09:51 +0300

    Update ConfigComments.java

--- plugin/src/main/java/sawfowl/localeapi/configure/localization/ConfigComments.java
+++ plugin/src/main/java/sawfowl/localeapi/configure/localization/ConfigComments.java
@@ -15,7 +15,7 @@ public class ConfigComments {
 				+ "Hocon - Стандартная конфигурация Sponge. Имеет некоторое сходство с Json. Поддерживает комментарии.\n"
 				+ "Json - Альтернативный вид конфигурации Sponge. Классический Json. Не поддерживает комментарии.\n"
 				+ "Yaml - Альтернативный вид конфигурации Sponge. Использует блочную структуру. Наиболее удобен для чтения человеку. На текущий момент не поддерживает комментарии, возможно будет исправленно в будущем командой Sponge.\n"
-				+ "GeyserYaml - Альтернативный вид конфигурации Sponge. Использует блочную структуру. Наиболее удобен для чтения человеку. Поддерживает комментарии. Поддержка данного формата является экспериментальной.\n"
+				+ "GeyserYaml - Альтернативный вид конфигурации Sponge. Использует блочную структуру. Наиболее удобен для чтения человеку. Поддерживает комментарии. Поддержка данного загрузчика является экспериментальной.\n"
 				+ "Jackson - Альтернативный вид конфигурации в формате json Sponge. Не поддерживает комментарии.\n"
 				+ "XML - Альтернативный вид конфигурации Sponge. Поддерживает комментарии, но не удобен для чтения.\n"
 				+ "Toml - Использование созданного в LocaleAPI загрузчика конфигурации формата Toml. Удобен для чтения. Поддерживает комментарии. Не рекомендуется для локализаций, так как при последующей конвертации из него в любой другой формат могут возникнуть ошибки из-за несовместимости Toml с форматом данных json.";
@@ -39,7 +39,7 @@ public class ConfigComments {
 			+ "Hocon - Standard Sponge configuration. Has some similarities with Json. Supports comments.\n"
 			+ "Json - Alternative configuration format. Classic Json. Does not support comments.\n"
 			+ "Yaml - An alternative Sponge configuration format that uses a block structure and is easy to read. Currently, it does not support comments, but this may be fixed in the future by the Sponge team.\n"
-			+ "GeyserYaml - An alternative Sponge configuration format that uses a block structure and is easy to read. It supports comments. Support for this format is experimental.\n"
+			+ "GeyserYaml - An alternative Sponge configuration format that uses a block structure and is easy to read. It supports comments. Support for this loader is experimental.\n"
 			+ "Jackson - An alternative Sponge configuration format in json format that does not support comments.\n"
 			+ "XML - An alternative Sponge configuration view that supports comments but is not easy to read.\n"
 			+ "Toml - Using the LocaleAPI-generated Toml format configuration loader. Easy to read. Supports comments. It is not recommended for localizations, as errors may occur during subsequent conversion from it to any other format due to the incompatibility of Toml with the json data format.";
